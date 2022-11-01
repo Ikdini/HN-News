2a1e82617bcda2c959d6d7c49255ec19dd4ea161
Dini <ioluwande@gmail.com>
2022-11-01 23:39:48 +0100

    cleaning TODO

--- news/tasks.py
+++ news/tasks.py
@@ -7,7 +7,6 @@ from . import views
 
 @shared_task
 def sync_stories_task():
-  # TODO: add a try catch block to handle the error
   # Get new stories from the API
   url = "/v0/newstories.json?print=pretty"
   decoded_100 = views.baseAPIConnection(url)[:100]
@@ -70,20 +69,14 @@ def sync_stories_task():
     print("NO NEW STORIES ADDED TO THE DATABASE!!!")
 
   # delete the old stories from the database
-  # TODO: edit this lines
-  print(Story.objects.all().count())
   if Story.objects.all().count() > 100:
-    all_stories = Story.objects.all()
-    old_stories = all_stories[100:]
-    print(old_stories.count())
+    old_stories = Story.objects.all()[100:]
     for story in old_stories:
       story.delete()
-  print(Story.objects.all().count())
 
 
 @shared_task
 def sync_jobs_task():
-  # TODO: add a try catch block to handle the error
   # Get new jobs from the API
   url = "/v0/jobstories.json?print=pretty"
   decoded_100 = views.baseAPIConnection(url)[:100]
@@ -143,12 +136,7 @@ def sync_jobs_task():
     print("NO NEW JOBS ADDED TO THE DATABASE!!!")
 
   # delete the old jobs from the database
-  # TODO: edit this lines
-  print(Job.objects.all().count())
   if Job.objects.all().count() > 100:
-    all_jobs = Job.objects.all()
-    old_jobs = all_jobs[100:]
-    print(old_jobs.count())
+    old_jobs = Job.objects.all()[100:]
     for job in old_jobs:
       job.delete()
-  print(Job.objects.all().count())
